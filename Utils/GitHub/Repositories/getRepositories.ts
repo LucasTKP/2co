f5cfbb0ac5133dc839fa265489295a897bf3007f
Gabriel Rodrigues <gabriel-rodriguesbs@hotmail.com>
2023-11-28 12:52:20 -0300

    fix: Removes unnecessary ternary operation

--- Utils/GitHub/Repositories/getRepositories.ts
+++ Utils/GitHub/Repositories/getRepositories.ts
@@ -1,31 +1,29 @@
-import { Octokit } from "@octokit/rest"
+import { Octokit } from '@octokit/rest';
 
 export default async function getrepositorys(token: string) {
+  const octokit = new Octokit({
+    auth: token,
+  });
 
-    const octokit = new Octokit({
-        auth: token
-    })
+  const response = await octokit.request('GET /user/repos', {
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28',
+    },
+    sort: 'created',
+    visibility: 'all',
+    affiliation: 'owner, collaborator, organization_member',
+  });
 
-    const response = await octokit.request('GET /user/repos', {
-        headers: {
-            'X-GitHub-Api-Version': '2022-11-28'
-        },
-        sort: 'created',
-        visibility: 'all',
-        affiliation: 'owner, collaborator, organization_member'
-    })
+  const repositorys = response.data.map((repository) => {
+    return {
+      id: repository.id.toString(),
+      avatar: repository.owner.avatar_url,
+      name: repository.name,
+      private: repository.visibility === 'private',
+      url: repository.homepage,
+      created_at: repository.created_at!,
+    };
+  });
 
-    const repositorys = response.data.map((repository) => {
-      return {
-        id:repository.id.toString(),
-        avatar:repository.owner.avatar_url,
-        name: repository.name,
-        private: repository.visibility === "private" ? true : false,
-        url: repository.homepage,
-        created_at: repository.created_at!
-      }  
-    })
-    
-    return repositorys
+  return repositorys;
 }
-
