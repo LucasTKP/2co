ac3e36e8644d0fd73eddac1bcd166fe39236839f
Gabriel Rodrigues <gabriel-rodriguesbs@hotmail.com>
2023-11-28 12:42:07 -0300

    refactor: refactores formatData util function

--- Utils/formatData.ts
+++ Utils/formatData.ts
@@ -1,9 +1,22 @@
+const months = [
+  'Janeiro',
+  'Fevereiro',
+  'Março',
+  'Abril',
+  'Maio',
+  'Junho',
+  'Julho',
+  'Agosto',
+  'Setembro',
+  'Outubro',
+  'Novembro',
+  'Dezembro',
+];
 export function FormatDate(date: number | undefined) {
-    if (date) {
-        const months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
-        let newDate = new Date(Number(date)).toLocaleDateString().split('/')
-        return `${newDate[0]} de ${months[Number(newDate[1]) - 1]} de ${newDate[2]}`
-    } else {
-        return "Data não inserida corretamente."
-    }
+  if (!date) return 'Data não inserida corretamente.';
+
+  let newDate = new Date(Number(date));
+  return `${newDate.getDate()} de ${
+    months[newDate.getMonth()]
+  } de ${newDate.getFullYear()}`;
 }
